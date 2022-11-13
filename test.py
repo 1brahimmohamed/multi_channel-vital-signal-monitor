921d3a35b618f1c9dbf2bbb2a933c1642b22c108
MohamedAIsmail <mohamedaismail214@gmail.com>
2022-11-13 23:54:01 +0200

    Added all the summation part

--- test.py
+++ test.py
@@ -8,6 +8,7 @@ import random
 from streamlit_option_menu import option_menu
 import hydralit_components as hc
 import plotly.express as px
+import numpy as np
 
 st.set_page_config(layout='wide', initial_sidebar_state='collapsed',)
 
@@ -37,8 +38,25 @@ if "x" not in st.session_state:
     st.session_state["x"] = []
     st.session_state["y"] = []
     st.session_state["i"] = 0
+    st.session_state["x_ctr"] = []
+    st.session_state['X_Bar'] = []
+    st.session_state["Range"] = []
+    st.session_state['X_BAR_Summation'] = 0
+    st.session_state['Range_Summation'] = 0
+    st.session_state['X_BAR_Average'] = 0
+    st.session_state['Range_Average'] = 0
+    st.session_state['X_Bar_Upper_Limit'] = 0
+    st.session_state['Range_Upper_Limit'] = 0
+    st.session_state['X_Bar_Lower_Limit'] = 0
+    st.session_state['Range_Lower_Limit'] = 0
+
 
 if (f"{menu_id}" == "Data Acquisiton"):
+    summation = 0
+    avg = 0
+    rangeList = []
+    Range = 0
+
     with leftCol:
         fig, ax = plt.subplots()
 
@@ -59,11 +77,42 @@ if (f"{menu_id}" == "Data Acquisiton"):
             graph_placeholder.plotly_chart(fig)
             st.session_state.i += 1
 
+            if(st.session_state.i % 3 == 0):
+
+                j = st.session_state.i-2
+                for j in range(st.session_state.i):
+                    summation = summation + st.session_state.y[j]
+                    rangeList.append(st.session_state.y[j])
+
+                Range = max(rangeList) - min(rangeList)
+                avg = summation / 3
+
+                st.session_state.X_Bar.append(avg)
+                st.session_state.X_BAR_Summation = st.session_state.X_BAR_Summation + avg
+                st.session_state.X_BAR_Average = st.session_state.X_BAR_Summation / \
+                    len(st.session_state.X_Bar)
+
+                st.session_state.Range.append(Range)
+                st.session_state.Range_Summation = st.session_state.Range_Summation + Range
+                st.session_state.Range_Average = st.session_state.Range_Summation / \
+                    len(st.session_state.Range)
+
+                st.session_state.X_Bar_Upper_Limit = st.session_state.X_BAR_Average + \
+                    (1.023 * st.session_state.Range_Average)
+                st.session_state.X_Bare_Lower_Limit = st.session_state.X_BAR_Average - \
+                    (1.023 * st.session_state.Range_Average)
+
+                st.session_state.Range_Upper_Limit = (
+                    2.575 * st.session_state.Range_Average)
+
+                st.session_state.x_ctr.append(len(st.session_state.X_Bar))
+
             plt.pause(1)
 
-# if (f"{menu_id}" == "Monitoring"):
-#     with leftCol:
-#         fig = px
-#         while True:
-#         fig2 = px.histogram(st.session_state.x, st.session_state.y)
-#         graph_placeholder.plotly_chart(fig2)
+if (f"{menu_id}" == "Monitoring"):
+    fig, ax = plt.subplots()
+
+    ax.plot(st.session_state.x_ctr, st.session_state.X_Bar,
+            color='blue', markersize=12)
+
+    graph_placeholder.plotly_chart(fig)
